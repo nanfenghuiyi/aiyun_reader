79159acdc35d02f9353a5476c701c2e7a8c46c11
rongruitao <ssdacs@qq.com>
2019-12-02 18:09:33 +0800

    页面调整

--- src/store/index.js
+++ src/store/index.js
@@ -3,24 +3,23 @@ import Vuex from 'vuex'
 
 // vuex 存数据，读数据
 Vue.use(Vuex)
-const key = 'user'
 const store = new Vuex.Store({
   // 要存的初始数据
   state: {
-    user: null,
-    cartProducts: []
+    // user: null,
+    // cartProducts: []
   },
   // 通过 mutations修改数据
   mutations: {
     // 设置数据 进入 localStorage
-    _setStorage(state, value) {
-      state.user = value
-      localStorage.setItem(key, JSON.stringify(value))  // localStroage.setItem(key,数据值) 设置存入缓存
-    },
-    _removeStorage(state) {
-      state.user = null
-      localStorage.removeItem(key)  // localStorage.removeItem(key) 删除缓存
-    }
+    // _setStorage(state, value) {
+    //   state.user = value
+    //   localStorage.setItem(key, JSON.stringify(value))  // localStroage.setItem(key,数据值) 设置存入缓存
+    // },
+    // _removeStorage(state) {
+    //   state.user = null
+    //   localStorage.removeItem(key)  // localStorage.removeItem(key) 删除缓存
+    // }
   },
   actions: {},
   // 通过getters 获取数据
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,145 +1,133 @@
 <template>
-  <div class="container">
+  <div class="container" v-loading="loading">
     <div v-if="totalShow" class="container-title" @touchmove.prevent>
       当前没有数据，请到宗族页面添加
     </div>
-    <div v-else class="swiper-page">
-      第{{page}}页 共{{total}}页
+    <div v-else >
+      <div class="menu" @click="menuPopup">
+        目录
+      </div>
+      <div class="swiper-page">
+        第{{page}}页 共{{total}}页
+      </div>
     </div>
     <swiper :options="swiperOption" ref="mySwiper" :class="{'swiper-no-swiping':totalShow}">
       <swiper-slide></swiper-slide>
       <swiper-slide class="swiper-style" v-for="count in bookListNum" :key="count">
         <div class="section-swiper">
-          <!-- <van-row justify="center" class="van-row" v-for="(item, i) of bookList" :key="i">
-            <van-col class="van-col border-right" span="4" >
-              <div class="border-bottem">世系</div>
-              <div style="height:60px" class="border-bottem">{{item.generation}}</div>
-              <div style="height:30px" class="border-bottem">兄妹 {{item.sibling_count}}人</div>
-              <div style="height:30px" class="border-bottem">{{item.rank}}</div>
-              <div style="height:30px" class="">个人简介</div>
-            </van-col>
-            <van-col class="van-col" span="8">
-              <div class="border-bottem border-right">本人</div>
-              <div class="border-right">
-                <div class="van-adress border-bottem">
-                  <div class="van-name">{{ item.self.name }}</div>
-                  <div class="van-title border-right border-left border-width">
-                    <div class="border-bottem">生</div>
-                    <div>卒</div>
-                  </div>
-                  <div  class="van-time">
-                    <div class="border-bottem">{{item.self.birthday!=''?item.self.birthday:'无'}}</div>
-                    <div>1990-01-12</div>
-                  </div>
-                </div>
-              </div>
-              <div class="border-bottem border-right user-img">
-                <img v-lazy="item.self.head_img!=null?item.self.head_img:'无'" alt="照片" />
-              </div>
-            </van-col>
-            <van-col class="van-col" span="8">
-              <div class="border-bottem border-right">配偶</div>
-              <div class="border-bottem">
-                <div class="van-adress border-right">
-                  <div class="van-name">张三三</div>
-                  <div class="van-title border-right border-left border-width">
-                    <div class="border-bottem">生</div>
-                    <div>卒</div>
-                  </div>
-                  <div class="van-time">
-                    <div class="border-bottem">{{item.couple!=null?item.couple.birthday:'无'}}</div>
-                    <div>无</div>
-                  </div>
-                </div>
-              </div>
-              <div class="border-bottem border-right user-img">
-                <img v-lazy="item.couple!=null?item.self.head_img:'无'" alt="照片" />
-              </div>
-            </van-col>
-            <van-col class="van-col" span="2">
-              <div class="border-bottem border-right">父母</div>
-              <div style="height:120px" class="border-bottem  border-right">
-                <ul v-for="(parent, index) of item.parents" :key="index" v-if="item.parents!=null">
-                  <li>{{parent.name}}</li>
-                </ul>
-              </div>
-            </van-col>
-            <van-col class="van-col" span="2">
-              <div class="border-bottem">孩子</div>
-              <div style="height:120px"  class="border-bottem">
-                <ul v-for="(child, index) of item.children" :key="index" v-if="item.children!=null">
-                  <li>{{child.name}}</li>
-                </ul>
-              </div>
-            </van-col>
-            <van-col class="van-col user-introduce" span="20">
-              <div class="user-introduce-style">个人简介
-              </div>
-            </van-col>
-          </van-row> -->
-          <table
-            border="1"
-            v-for="(item, i) of bookList"
-            :key="i">
+          <table border="1" cellspacing="0" cellpadding="0" v-for="(item, i) of bookList" :key="i">
             <tr>
               <th>世系</th>
               <th colspan="4">本人</th>
-              <th colspan="4">配偶</th>
               <th>父母</th>
               <th>孩子</th>
             </tr>
             <tr>
-              <td rowspan="4">{{item.generation}}</td>
+              <td rowspan="3" class="generation">{{item.generation}}</td>
               <td rowspan="2">{{ item.self.name }}</td>
               <td>生</td>
               <td colspan="2">{{item.self.birthday!=''?item.self.birthday:'无'}}</td>
-              <td rowspan="2">{{item.couple!=null?item.couple.name:'无'}}</td>
-              <td>生</td>
-              <td colspan="2">{{item.couple!=null?item.couple.birthday:'无'}}</td>
-              <td rowspan="6">
-                <ul v-for="(parent, index) of item.parents" :key="index" v-if="item.parents!=null">
-                  <li>{{parent.name}}</li>
-                </ul>
+              <td rowspan="2">
+                <div class="child-style-parent">
+                  <ul v-for="(parent, index) of item.parents" :key="index" v-if="item.parents!=null">
+                    <li>{{parent.name}}</li>
+                  </ul>
+                </div>
               </td>
-              <td rowspan="6" class="child-style">
-                <ul v-for="(child, index) of item.children" :key="index" v-if="item.children!=null">
-                  <li>{{child.name}}</li>
-                </ul>
+              <td rowspan="6">
+                <div class="child-style">
+                  <ul v-for="(child, index) of item.children" :key="index" v-if="item.children!=null">
+                    <li>{{child.name}}</li>
+                  </ul>
+                </div>
               </td>
             </tr>
             <tr>
               <td>卒</td>
               <td colspan="2">{{item.s1}}</td>
-              <td>卒</td>
-              <td colspan="2">{{item.s2}}</td>
             </tr>
-            <tr class="section-table-img">
+            <tr>
               <td colspan="4" rowspan="4">
-                <img v-lazy="item.self.head_img" alt="照片" />
+                <div class="section-table-img">
+                  <img v-lazy="item.self.head_img" alt="照片" />
+                </div>
               </td>
-              <td colspan="4" rowspan="4">
-                <img v-lazy="item.couple!=null?item.self.head_img:'无'" alt="照片" />
+              <td class="table-th">
+                配偶
               </td>
             </tr>
             <tr></tr>
             <tr>
               <td>兄妹 {{item.sibling_count}}人</td>
+              <td rowspan="2">
+                {{item.couple!=null?item.couple.name:'无'}}
+              </td>
             </tr>
             <tr>
-              <td>{{item.rank}}</td>
+              <td>{{item.rank!=''?item.rank:'无'}}</td>
             </tr>
             <tr>
               <td>本人简介</td>
-              <td colspan="10" class="user-td-introduce">{{item.self.brief}}</td>
+              <td colspan="6">
+                <div class="user-td-introduce">
+                  {{item.self.brief}}
+                </div>
+              </td>
             </tr>
           </table>
         </div>
       </swiper-slide>
       <swiper-slide>
-        <van-button class="btn" color="#EAC257" @click="goFirstPage">点击回到第 1 页</van-button>
+        <div class="slide-btn">
+          <van-button class="btn" color="#EAC257" @click="goFirstPage">点击回到当前首页</van-button>
+          <br>
+          <van-button class="btn" color="#EAC257" @click="goInitialPage">点击回到初始页</van-button>
+        </div>
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
+    <!-- 目录 -->
+    <div class="section-genealogy">
+      <van-popup v-model="genealogyShow" position="left" :style="{ with: '100%', height: '100%'}">
+        <div class="genealogy-header">
+          <p>世 系 目 录</p>
+        </div>
+        <div class="genealogy-search">
+          <van-search
+            v-model="searchValue"
+            placeholder="请输入姓名"
+            show-action
+            shape="round"
+            @input="onSearch(1)"
+            @clear="onClear">
+            <div slot="action" @click="onCancel(1)">取消</div>
+          </van-search>
+        </div>
+        <!-- <van-button class="goback-btn" color="#EAC257" @click="goInitialPage">回到初始页</van-button> -->
+        <div class="genealogy-section">
+          <div class="goback-btn" @click="goInitialPage">回到初始页</div>
+          <ul v-for="(item, index) of getGenealogyList" :key="index">
+            <li @click="getGenealogy(item.depth, index)">
+              {{item.title}}
+              <!-- <ul>
+                <li>112</li>
+                <li>112</li>
+              </ul> -->
+            </li>
+          </ul>
+        </div>
+        <!-- 搜索结果显示 -->
+        <div v-show="searchShow" class="res-show">
+          <ul v-infinite-scroll="onLoad" infinite-scroll-disabled="disabled" infinite-scroll-immediate="false" infinite-scroll-delay="10">
+            <li :class="{'genealogyActive':genealogyChecked.indexOf(index)>-1}"
+              v-for="(item, index) of searchList" :key="index" @click="getSearch(item.user_id, index)">
+              {{item.name}}
+              <span class="search-father">父亲 {{item.father_name}}</span>
+            </li>
+          </ul>
+        </div>
+      </van-popup>
+    </div>
   </div>
 </template>
 
@@ -182,34 +170,49 @@ export default {
         },
       },
       token: '',
+      depth: 0,
+      user_id: '',
       swiperindex: 0,
       bookListNum: 1, // 页数
       bookList: [],
       page: 1,
       total: 1,
-      totalShow: false,
+      totalShow: false, // 内容显示
+      genealogyShow: false, // 目录显示 
+      getGenealogyList: [], // 目录列表
+      searchValue: '', // 搜索value
+      searchShow: false , // 搜索结果显示
+      searchList: [], // 搜索结果列表
+      loadPage: 1, // 搜索的page
+      genealogyChecked: [], // 点击后的状态
+      disabled: false, // 滚动禁用
+      loading: false, // 加载状态
     };
   },
   methods: {
     // 数据获取
     loadMore(e) {
-      JsInterface.showAndroidLoading();
+      this.loading = true; // 加载状态
       this.$refs.mySwiper.swiper.slideTo(1, 1, true);
       var url='/Web/book/page';
       if (e==1) {
+        console.log('数据获取===')
         var page = this.page;
-      }else {
+      }else if(e==2){
+        // console.log('返回当前首页===')
         var page = 1;
-        // this.$store.commit('pageStorage',{page: page});
         this.page = page;
+      }else {
+        // console.log('返回初始状态===')
+        var page = 1;
+        this.page = page;
+        this.depth = 0;
+        this.user_id = '';
       }
-      // var obj={page: page, page_size: 3, token: 'fgex7T7PIXJ9bZxBPw1SMf65WbfUZC/L'}
-      // var token = window.getUserId ? window.getUserId() :  window.JsInterface.getUserId();
-      // if (window.getUserId != null && window.getUserId != undefined) {
-      //   var token = window.getUserId();
-      //   console.log('loadMore===进入后',token)
-      // }
-      var obj={page: page, page_size: 3, token: this.token}
+      // 测试
+      var obj={page: page, page_size: 3, depth: this.depth, user_id: this.user_id, token: 'P62_K_Vve325khpTB/QX0Q=='} 
+      
+      // var obj={page: page, page_size: 3, depth: this.depth, token: this.token}
       console.log(obj)
       this.axios.post(url,obj)
       .then(res=>{
@@ -219,49 +222,145 @@ export default {
         if (data.data.total>0) {
           this.totalShow = false;
         }else {
-          this.totalShow = true
+          this.totalShow = true;
         }
-        JsInterface.dismissAndroidLoading();
-        this.bookList = data.data.records
-      }).catch(err=>
-        JsInterface.dismissAndroidLoading(),
-        // console.log(err)
-      )
+        this.bookList = data.data.records;
+        this.loading = false;
+      }).catch(err=>{
+        console.log(err)
+      })
     },
-    // 返回第一页
+    // 返回当前首页
     goFirstPage() {
       this.loadMore(2);
       this.$refs.mySwiper.swiper.slideTo(1, 1, true);
     },
+    // 返回初始页
+    goInitialPage() {
+      this.genealogyShow = false;
+      this.loadMore(3);
+      this.$refs.mySwiper.swiper.slideTo(1, 1, true);
+    },
+    // 安卓获取token
     getToken() {
-      console.log("JsInterface===",JsInterface)
+      // console.log("JsInterface===",JsInterface)
       this.token = JsInterface.getToken();
       this.loadMore(1)
       console.log("getToken token = " + this.token);
     },
-  },
-  created() {
-    // console.log(window.getUserId())
-    // this.token = window.getUserId();
-    // this.loadMore(1);
+    // 获取目录
+    menuGetGenealogy() {
+      var url = '/Web/Book/contents';
+      // 测试
+      var obj = {token:'P62_K_Vve325khpTB/QX0Q=='};
+      
+      // var obj = {token: this.token};
+      this.axios.post(url, obj)
+      .then(res=>{
+        var data = res.data.data;
+        this.getGenealogyList = data.records
+      })
+    },
+    // 目录选中
+    getGenealogy(depth, index) {
+      this.depth = depth;
+      this.page = 1;
+      this.user_id = '';
+      this.loadMore(1);
+      this.genealogyShow = false;
+    },
+    // 目录显示
+    menuPopup() {
+      this.genealogyShow = true;
+    },
+    // 搜索
+    onSearch(page) {
+      this.searchShow = true;
+      this.disabled = false;
+      if (this.searchValue != '') {
+        var url = '/web/Book/search';
+        // 测试
+        var obj = {keyword:this.searchValue, page:page, page_size: 20, token:'P62_K_Vve325khpTB/QX0Q=='};
+        
+        // var obj = {keyword:this.searchValue, page:page, page_size: 20, token: this.token};
+        this.axios.post(url, obj)
+        .then(res => {
+          // console.log('确定搜索时触发===',res);
+          var data = res.data;
+          // console.log(data.data.records)
+          if (data.data.records != [] && data.data.records.length !=0 ) {
+            if (page>1) {
+              this.searchList=this.searchList.concat(data.data.records)
+            }else {
+              this.searchList = data.data.records;
+            }
+          }else {// 阻止滚动事件
+            // console.log('阻止滚动事件===')
+            this.disabled = true;
+          }
+        })
+      }
+    },
+    // 取消搜索
+    onCancel() {
+      this.searchValue = '';
+      this.searchShow = false;
+      this.searchList = [];
+      this.loadPage = 1;
+      this.genealogyChecked = [];
+    },
+    // 清除按钮
+    onClear() {
+      this.searchShow = false;
+      this.searchList = [];
+      this.loadPage = 1;
+      this.genealogyChecked = [];
+    },
+    // 搜索结果滚动
+    onLoad () {
+      var page=this.loadPage++;
+      if (this.searchList.length>=20) {
+        this.onSearch(page);
+      }
+    },
+    //点击搜索结果
+    getSearch(user_id, index) {
+      this.user_id = user_id;
+      this.loadMore(1);
+      this.genealogyShow = false;
+      // 选择后的样式改变
+      if(this.genealogyChecked.indexOf(index) == -1) {
+        this.genealogyChecked.push(index); //实现第一种效果只需要这句代码  实现第二种效果就要加上外面的判断条件 
+      }else {
+        var spliceIndex = this.genealogyChecked.indexOf(index);
+        this.genealogyChecked.splice(spliceIndex, 1);
+      }
+    }
   },
   mounted() {
-    // 初始化页面
+    // 获取当前窗口的高度
+    console.log('获取当前窗口的高度==',$(window).height())
+    console.log('获取当前窗口的80%高度==',$(window).height()*0.8)
+
+    // 测试
+    this.loadMore(1);
+    this.menuGetGenealogy();
+    
+    /* // 初始化页面
     var u = navigator.userAgent;
-    console.log('终端判断===',u)
+    // console.log('终端判断===',u)
     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
     var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
     if (isAndroid) {
       this.getToken();
+      JsInterface.dismissAndroidLoading();
+      this.menuGetGenealogy();
     }else if (isiOS) {
       this.token = window.getUserId();
       this.loadMore(1);
-    }
-    
+      this.menuGetGenealogy();
+    } */
   },
-  beforeUpdate () {
-    console.log('当前数据发生更改')
-  }
 };
 </script>
 
@@ -269,9 +368,14 @@ export default {
 td {
   padding: 0;
 }
+.table-th{
+  font-weight: bold;
+  text-align: -internal-center;
+}
 .container {
   width: 100%;
   height: 100%;
+  font-size: 8px;
 }
 .container-title{
   position: fixed;
@@ -296,7 +400,7 @@ td {
   width: 20px;
 } */
 .swiper-style td {
-  width: 20px;
+  width: 30px;
   height: 20px;
 }
 .swiper-page{
@@ -310,84 +414,117 @@ td {
   margin-top: 5px;
   table-layout: fixed;
 }
-.section-table-img > td > img {
-  width: 60px;
+.generation{
+  font-size: 14px;
+  font-weight: 600;
+}
+.section-table-img,.section-table-img > img {
+  /* width: 80px; */
   height: 60px;
+  margin: 0 auto;
 }
 .child-style{
+  height: 100px;
+  overflow-y: scroll;
+}
+.child-style-parent{
+  height: 40px;
   overflow-y: scroll;
 }
 .user-td-introduce{
+  width: 100%;
+  height: 39px;
+  text-align: left;
   overflow-y: scroll;
 }
-
-/* 弹性布局 */
-.van-row{
-  height: 30%;
-  border: 1px solid red;
-  text-align: center;
-  margin-top: 5px;
+.slide-btn{
+  margin-top: 40%;
 }
-.van-col{
-  height: 80%;
-  box-sizing: border-box;
-  background: #0ff;
+.btn{
+  width: 50%;
+  margin-bottom: 20px;
 }
-.border-width{
-  width: 20px;
+.goback-btn{
+  color: #EAC257;
 }
-.border-bottem{
-  border-bottom: 1px solid red;  
-  box-sizing: border-box;
-}
-.border-right{
-  border-right: 1px solid red;  
-  box-sizing: border-box;
-}
-.border-left{
-  border-left: 1px solid red;  
-  box-sizing: border-box;
-}
-.van-adress{
+/* 目录 */
+.genealogy-header {
+  width: 250px;
+  height: 40px;
+  line-height: 40px;
+  text-align: left;
+  font-size: 18px;
+  background: rgba(252,251,231,1);
   display: flex;
-  justify-content:space-between;
   align-items: center;
 }
-.van-name,.van-time{
+.el-icon-arrow-left {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+}
+.genealogy-header > p {
   flex: 1;
+  text-align: center;
+  font-size: 20px
 }
-.van-title{
-  flex: .5;
+.genealogy-section {
+  width: 250px;
+  height: 350px;
+  background: rgba(252,251,231,1);
+  overflow: auto;
 }
-.user-img{
-  height: 74px;
+.genealogy-section li, .res-show li, .goback-btn{
+  line-height: 30px;
+  font-size: 16px;
+  border-bottom: 1px solid #eee;
+  /* background: #0ff; */
 }
-.user-img img{
-  width: 100%;
-  height: 100%;
-}
-.user-introduce{
-  display: flex;
-}
-.user-introduce-style{
-  width: 100%;
+.menu{
+  position:fixed;
+  top: 2px;
+  left: 15px;
+  width: 30px;
   height: 30px;
-  overflow-y: auto;
+  line-height: 30px;
+  border-radius: 50%;
+  font-size: 12px;
+  background: #EAC257;
+  z-index: 999;
 }
-
-
-.btn{
-  margin-top: 80%;
+/* 点击后的样式 */
+.genealogyActive {
+  color: #ccc;
 }
-.progress{
-  width: 95%;
-  border: 1px solid red;
-
+/* 搜索框 */
+.genealogy-search{
+  width: 250px;
+}
+/* 搜索结果 */
+.res-show{
+  position: fixed;
+  top: 90px;
+  left: 0px;
+  width: 250px;
+  height: 350px;
+  background: rgba(252,251,231,1);
+  overflow-y: scroll; 
+}
+.search-father{
+  font-size: 10px;
+  color: #664237;
 }
 </style>
 
 <style>
 .van-overlay {
   background-color: rgba(0, 0, 0, 0.1) !important;
+}
+.van-popup--left{
+  background: rgba(252,251,231,1) !important;
 }
 </style>
